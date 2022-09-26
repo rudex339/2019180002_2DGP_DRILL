85aead0b2c1f586ff69729c028509443f5106127
onehana1 <73771420+onehana1@users.noreply.github.com>
2022-09-26 14:22:47 +0900

    Update character_grass.py

--- 06/character_grass.py
+++ 06/character_grass.py
@@ -5,39 +5,39 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 #fist start x = 400 y = 0
-
-
-while(1):
-    x=400
-    y=0
-    check = 1
-    while(check):
+def render_all(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.01)
+def run_circle():
+    clear_canvas_now()
+    grass.draw_now(400,30)    
+    cx, cy, r = 400,290,200
+    for deg in range(-90,270,5):
         clear_canvas_now()
         grass.draw_now(400,30)
-        character.draw_now(x,y+90)
-        if(x<800 and y==0):
-            x=x+2
-            if(x==400 and y==0):
-                check = 0
-        elif(y<510 and x==800):
-            y=y+2
-        elif(x>0 and y==510):
-            x=x-2
-        elif(x==0 and y > 0):
-            y= y -2
-        delay(0.01)
+        x= cx+r*math.cos(deg/360*2*math.pi)
+        y= cy+r*math.sin(deg/360*2*math.pi)
+        render_all(x,y)        
+    pass
 
-    check = 1
-    y= 90
-    count = -90
-    while(check):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        x =400+100*math.cos(math.pi*(count/180))
-        y = 190+100*math.sin(math.pi*(count/180))
-        count = count +2
-        if(count==270):
-            check = 0
-        delay(0.01)
+def run_rectangle():
+    print("rectangle")
+    for x in range(400,750+1,10):
+        render_all(x,90)
+    for y in range(90,550+1,10):
+       render_all(750,y)
+    for x in range(750,50-1,-10):
+        render_all(x,550)
+    for y in range(550,90-1,-10):
+       render_all(50,y)
+    for x in range(50,400+1,10):
+       render_all(x,90)
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+
 close_canvas()
